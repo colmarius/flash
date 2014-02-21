07e7efdb487af4effac970940b049a244e47930b
Marius Colacioiu <marius.colacioiu@gmail.com>
2014-02-21 22:03:05 +0100

    Better error for missing Runfile.

--- lib/flash/runfile.rb
+++ lib/flash/runfile.rb
@@ -5,6 +5,11 @@ require 'delegate'
 class Flash::Runfile < SimpleDelegator
 
   def initialize(filename=nil)
+    unless File.exists?(filename)
+      puts "ERROR: missing Runfile"
+      exit 1
+    end
+
     data = filename ? YAML.load_file(filename)  : {}
     super(data)
   end
