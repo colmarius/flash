003e0c6a0de5092933ac1d24a8c6cbae4010fdd0
Dario Ghilardi <darioghilardi@webrain.it>
2014-03-29 13:30:44 +0100

    Directly pass args in the executable.

--- lib/flash/cli.rb
+++ lib/flash/cli.rb
@@ -4,20 +4,19 @@ require 'pathname'
 
 class Flash::CLI
 
-  def self.start
-    if ARGV.empty?
+  def self.start(params)
+    if params.empty?
       puts "ERROR: Flash requires a command to run."
       exit 1
     end
 
-    command = ARGV[0].to_s.downcase
+    command = params[0].to_s.downcase
 
     case command
     when 'run'
-      Flash::Run.new(ARGV).start
+      Flash::Run.new(params).start
     else
       puts "ERROR: Flash does not known command: #{command}."
     end
   end
-
 end
