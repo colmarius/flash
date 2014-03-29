a5aace8c5cfe28cf369abc02297f7349e3a1a6d2
Dario Ghilardi <darioghilardi@webrain.it>
2014-03-29 16:05:00 +0100

    Added a class to handle unkjown commands.

---
+++ lib/flash/unknown_command.rb
@@ -0,0 +1,18 @@
+require 'flash'
+
+class Flash::UnknownCommand
+  def initialize(args)
+    @command = args[0]
+  end
+
+  def start
+    puts "ERROR: Flash does not know command: #{@command}."
+    shutdown
+  end
+
+  private
+
+  def shutdown
+    exit 1
+  end
+end
