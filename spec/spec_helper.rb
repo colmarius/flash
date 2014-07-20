5c241b4b287ed9397651f7859747e56a80c65178
Marius Colacioiu <marius.colacioiu@gmail.com>
2014-07-20 12:06:01 +0200

    Reorganize simplecov start clause.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,11 @@
-require 'simplecov'
-SimpleCov.start do
-  add_filter '/spec'
-  add_filter '/vendor'
-end if ENV['COVERAGE']
+if ENV['COVERAGE']
+  require 'simplecov'
+
+  SimpleCov.start do
+    add_filter '/spec'
+    add_filter '/vendor'
+  end
+end
 
 require 'rspec'
 
