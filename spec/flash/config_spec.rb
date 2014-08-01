39563346b835ba7873a8641ae48ec103f4b4f2b1
Marius Colacioiu <marius.colacioiu@gmail.com>
2014-08-02 00:01:50 +0200

    Add spec for no config file found.

--- spec/flash/config_spec.rb
+++ spec/flash/config_spec.rb
@@ -8,4 +8,12 @@ describe Flash::Config, :fakefs  do
     write_config_file
     expect(subject['projects']).to eq %w(foo bar buz)
   end
+
+  it 'will exit with error message if no config file found' do
+    output = capture_stdout do
+      expect { subject }.to raise_error SystemExit
+    end
+
+    expect(output).to match(/ERROR: .flash.yml does not exist.\n/)
+  end
 end
