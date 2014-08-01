3a818d7c46dc55f1d73e60f3aa1d91bd1a3a5066
Marius Colacioiu <marius.colacioiu@gmail.com>
2014-08-01 23:22:42 +0200

    Flash config can load from a file.

--- spec/flash/config_spec.rb
+++ spec/flash/config_spec.rb
@@ -2,5 +2,10 @@ require 'spec_helper'
 require 'flash/config'
 
 describe Flash::Config do
+  subject { Flash::Config.new }
 
+  it 'can load from a file' do
+    write_config_file
+    expect(subject['projects']).to eq %w(foo bar buz)
+  end
 end
