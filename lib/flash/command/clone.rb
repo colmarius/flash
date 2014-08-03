2122a0989055d73cb9cd681dffea0de75bc9ef44
Marius Colacioiu <marius.colacioiu@gmail.com>
2014-08-03 17:52:30 +0200

    Handle case when missing git clone URL.

--- lib/flash/command/clone.rb
+++ lib/flash/command/clone.rb
@@ -10,6 +10,7 @@ class Flash::Command::Clone < Flash::Command::Base
     raise(ArgumentError, 'Missing required group parameter.') unless @group
 
     unknown_group_and_exit(@group) unless valid_group?(@group)
+    missing_git_clone_url unless has_clone_git_url?
     clone_projects(@group)
   end
 
@@ -18,6 +19,16 @@ class Flash::Command::Clone < Flash::Command::Base
   def unknown_group_and_exit(group)
     puts "Unknown group \"#{group}\" in .flash.yml config."
     exit 1
+  end
+
+  def missing_git_clone_url
+    puts 'Missing clone git URL defined in .flash.yml config.'
+    exit 1
+  end
+
+  def has_clone_git_url?
+    clone_git_url = (config['clone'] && config['clone']['git'])
+    clone_git_url ? true : false
   end
 
   def clone_projects(group)
