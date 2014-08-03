f380a86933a8cf7c3d1c69ca8cb8415462cf943e
Marius Colacioiu <marius.colacioiu@gmail.com>
2014-08-03 11:07:34 +0200

    Add spec for Thor patch.
    Allow "run" to be defined as command.

--- spec/flash/cli_spec.rb
+++ spec/flash/cli_spec.rb
@@ -2,5 +2,11 @@ require 'spec_helper'
 require 'flash/cli'
 
 describe Flash::CLI do
+  subject { Flash::CLI }
 
+  describe '.is_thor_reserved_word?' do
+    it 'should allow "run" to be defined as command' do
+      expect(subject.is_thor_reserved_word?('run', :command)).to be_falsey
+    end
+  end
 end
