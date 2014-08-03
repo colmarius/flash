10195e41313b7c022e4539641179ac5cd125fcd0
Marius Colacioiu <marius.colacioiu@gmail.com>
2014-08-03 11:11:14 +0200

    Add spec for flash version command.

--- spec/flash/cli_spec.rb
+++ spec/flash/cli_spec.rb
@@ -9,4 +9,12 @@ describe Flash::CLI do
       expect(subject.is_thor_reserved_word?('run', :command)).to be_falsey
     end
   end
+
+  describe '#version' do
+    let(:output) { flash('version') }
+
+    it 'should print version number' do
+      expect(output).to match(/#{Flash::VERSION}\n/)
+    end
+  end
 end
