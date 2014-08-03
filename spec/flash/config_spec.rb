bfe92d18424378375739579c6596a72b0f1fb977
Marius Colacioiu <marius.colacioiu@gmail.com>
2014-08-03 10:32:08 +0200

    Specs describe Flash::Config#initialize.

--- spec/flash/config_spec.rb
+++ spec/flash/config_spec.rb
@@ -4,16 +4,17 @@ require 'flash/config'
 describe Flash::Config, :fakefs  do
   subject { Flash::Config.new }
 
-  it 'can load from a file' do
-    write_config_file
-    expect(subject['projects']).to eq %w(foo bar buz)
-  end
-
-  it 'will exit with error message if no config file found' do
-    output = capture_stdout do
-      expect { subject }.to raise_error SystemExit
+  describe '#initialize' do
+    it 'can load from a file' do
+      write_config_file
+      expect(subject['projects']).to eq %w(foo bar buz)
     end
 
-    expect(output).to match(/ERROR: .flash.yml does not exist.\n/)
+    it 'will exit with error message if no config file found' do
+      output = capture_stdout do
+        expect { subject }.to raise_error SystemExit
+      end
+      expect(output).to match(/ERROR: .flash.yml does not exist.\n/)
+    end
   end
 end
