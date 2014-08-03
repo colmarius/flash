33c8b0e343bceb666acbbb2b26962b0a9802a845
Marius Colacioiu <marius.colacioiu@gmail.com>
2014-08-03 10:56:00 +0200

    Add specs for Flash::Command::Base.

--- spec/flash/command/base_spec.rb
+++ spec/flash/command/base_spec.rb
@@ -1,6 +1,31 @@
 require 'spec_helper'
 require 'flash/command/base'
 
-describe Flash::Command::Base do
+describe Flash::Command::Base, :fakefs do
+  subject { Flash::Command::Base.new }
 
+  before { write_config_file }
+
+  describe '#config' do
+    let(:config) { subject.config }
+
+    it 'should be of correct kind' do
+      expect(config).to be_an_instance_of(Flash::Config)
+    end
+
+    it 'should behave as Hash' do
+      expect(config).to respond_to(:[])
+    end
+  end
+
+  describe '#valid_group?' do
+    it 'responds true for valid groups' do
+      expect(subject.valid_group?('projects')).to be_truthy
+      expect(subject.valid_group?('libraries')).to be_truthy
+    end
+
+    it 'responds false otherwise' do
+      expect(subject.valid_group?('unknkown')).to be_falsey
+    end
+  end
 end
