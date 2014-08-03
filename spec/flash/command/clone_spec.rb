976d1b67f5b67c44b4eaa372a09bec4b8b412b36
Marius Colacioiu <marius.colacioiu@gmail.com>
2014-08-03 17:53:04 +0200

    Add spec for Flash::Command::Clone.

--- spec/flash/command/clone_spec.rb
+++ spec/flash/command/clone_spec.rb
@@ -1,6 +1,66 @@
 require 'spec_helper'
 require 'flash/command/clone'
 
-describe Flash::Command::Clone do
+describe Flash::Command::Clone, :fakefs do
+  subject { described_class.new(@group) }
 
+  before { write_config_file }
+
+  describe '#execute' do
+
+    context 'with invalid group' do
+      it 'with nil group' do
+        @group = nil
+        expect { subject.execute }.to raise_error ArgumentError
+      end
+
+      it 'with empty group' do
+        @group = ''
+        output = capture_stdout do
+          expect { subject.execute }.to raise_error SystemExit
+        end
+        expect(output).to match(/Unknown group "" in .flash.yml config./)
+      end
+
+      it 'with unknown group' do
+        @group = 'unknown'
+        output = capture_stdout do
+          expect { subject.execute }.to raise_error SystemExit
+        end
+        expect(output).to match(/Unknown group "unknown" in .flash.yml config./)
+      end
+    end
+
+    context 'with valid group' do
+      before { @group = 'projects' }
+
+      describe 'without clone git endpoint' do
+        it 'should raise error' do
+          output = capture_stdout do
+            expect { subject.execute }.to raise_error SystemExit
+          end
+          expect(output).to match(/Missing clone git URL defined in .flash.yml config./)
+        end
+      end
+
+      describe 'with clone git endpoint' do
+        before do
+          contents = <<-CONTENTS
+            projects:
+              - foo
+              - bar
+              - buz
+            clone:
+              git: git@github.com:SuperDuper
+          CONTENTS
+          write_config_file(contents: contents)
+        end
+
+        it 'should clone projects in group' do
+          expect(subject).to receive(:clone_projects).with(@group).once
+          subject.execute
+        end
+      end
+    end
+  end
 end
